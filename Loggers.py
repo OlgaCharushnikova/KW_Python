58bdd9cc7f98a75dc63a829e92d53d326b04ec85
Olga Charushnikova <olga_charushnikova@bk.ru>
2023-10-17 00:30:49 +0300

    Добавление заметки

--- Loggers.py
+++ Loggers.py
@@ -2,9 +2,26 @@ import csv
 import os
 import datetime
 def read_notes():
-    with open("notes.csv", encoding='utf-8') as r_file:
-        file_reader = csv.DictReader(r_file, delimiter = ";")
-        for row in file_reader:
+    with open("notes.csv", encoding='utf-8') as f:
+        file= csv.DictReader(f, delimiter = ";")
+        for row in file:
             for key, value in row.items():
                 print("{0}: {1}".format(key,value))
             print('-----------------------------------')
+
+
+def add_note():
+    with open("notes.csv", mode="a", encoding='utf-8') as f:
+        new_entry = ["Id", "Название","Тело", "Дата"]
+        file = csv.DictWriter(f, delimiter = ";", 
+                                    lineterminator="\r", fieldnames=new_entry)
+        new_Id = input('Введите Id: ')
+        for row in file:
+            if row["Id"] == new_Id:
+                    print('Такой Id уже существует, введите новый Id: ')
+                    new_Id = input('Введите Id: ')
+        new_title = input('Введите название: ')
+        new_body = input('Введите тело заметки: ')
+        new_date = datetime.datetime.now().strftime("%Y-%m-%d")
+        file.writeheader()
+        file.writerow({"Id": new_Id, "Название": new_title, "Тело": new_body, "Дата" : new_date})    
