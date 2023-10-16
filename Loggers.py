b7503aa9d43e9fef27b0ef3ca211e1e8099bd401
Olga Charushnikova <olga_charushnikova@bk.ru>
2023-10-17 01:32:31 +0300

    корректировка

--- Loggers.py
+++ Loggers.py
@@ -1,25 +1,23 @@
 import csv
 import os
 import datetime
-def read_notes():
-    with open("notes.csv", encoding='utf-8') as f:
-        file= csv.DictReader(f, delimiter = ";")
-        for row in file:
-            for key, value in row.items():
-                print("{0}: {1}".format(key,value))
-            print('-----------------------------------')
 
 
-def add_note():
-    with open("notes.csv", mode="a", encoding='utf-8') as f:
+# def read_notes():
+#     with open("notes.csv", encoding='utf-8') as f:
+#         file= csv.DictReader(f, delimiter = ";")
+#         for row in file:
+#             for key, value in row.items():
+#                 print("{0}: {1}".format(key,value))
+#             print('-----------------------------------')
+
+
+# def add_note():
+with open("notes.csv", mode="a", encoding='utf-8') as f:
         new_entry = ["Id", "Название","Тело", "Дата"]
         file = csv.DictWriter(f, delimiter = ";", 
                                     lineterminator="\r", fieldnames=new_entry)
         new_Id = input('Введите Id: ')
-        for row in file:
-            if row["Id"] == new_Id:
-                    print('Такой Id уже существует, введите новый Id: ')
-                    new_Id = input('Введите Id: ')
         new_title = input('Введите название: ')
         new_body = input('Введите тело заметки: ')
         new_date = datetime.datetime.now().strftime("%Y-%m-%d")
@@ -28,65 +26,65 @@ def add_note():
 
 
 
-def del_note():
-    del_Id = input('Введите Id: ')
-    with open("notes.csv", encoding='utf-8') as f:
-        file = csv.DictReader(f, delimiter = ";")
-        for row in file:
-            if row["Id"] != del_Id and row["Id"] != 'Id':
-                with open("new_notes.csv", mode="a", encoding='utf-8') as ff:
-                    new_entry = ["Id", "Название","Тело", "Дата"]
-                    ffile = csv.DictWriter(ff, delimiter = ";", 
-                                                lineterminator="\r", fieldnames=new_entry)
-                    ffile.writeheader()
-                    ffile.writerow({"Id": row["Id"], "Название": row["Название"], "Тело": row["Тело"], "Дата" : row["Дата"]})
-    os.remove("notes.csv")
-    os.rename("new_notes.csv", "notes.csv")  
+# def del_note():
+#     del_Id = input('Введите Id: ')
+#     with open("notes.csv", encoding='utf-8') as f:
+#         file = csv.DictReader(f, delimiter = ";")
+#         for row in file:
+#             if row["Id"] != del_Id and row["Id"] != 'Id':
+#                 with open("new_notes.csv", mode="a", encoding='utf-8') as ff:
+#                     new_entry = ["Id", "Название","Тело", "Дата"]
+#                     ffile = csv.DictWriter(ff, delimiter = ";", 
+#                                                 lineterminator="\r", fieldnames=new_entry)
+#                     ffile.writeheader()
+#                     ffile.writerow({"Id": row["Id"], "Название": row["Название"], "Тело": row["Тело"], "Дата" : row["Дата"]})
+#     os.remove("notes.csv")
+#     os.rename("new_notes.csv", "notes.csv")  
 
 
-def edit_note():
-    del_Id = input('Введите Id: ')
-    with open("notes.csv", encoding='utf-8') as f:
-        file = csv.DictReader(f, delimiter = ";")
-        new_title = input('Введите новое название: ')
-        new_body = input('Введите новое тело заметки: ')
-        new_date = datetime.datetime.now().strftime("%Y-%m-%d")
-        with open("new_notes.csv", mode="w", encoding='utf-8') as ff:
-                new_entry = ["Id", "Название","Тело", "Дата"]
-                ffile = csv.DictWriter(ff, delimiter = ";", 
-                                            lineterminator="\r", fieldnames=new_entry)
-                file.writeheader()
-                file.writerow({"Id": del_Id, "Название": new_title, "Тело": new_body, "Дата" : new_date})
-        for row in file:        
-            if row["Id"] != del_Id and row["Id"] != 'Id':
-                with open("new_notes.csv", mode="a", encoding='utf-8') as w_file:
-                    new_entry = ["Id", "Название","Тело", "Дата"]
-                    file_writer = csv.DictWriter(w_file, delimiter = ";", 
-                                            lineterminator="\r", fieldnames=new_entry)
-                    file_writer.writeheader()
-                    file_writer.writerow({"Id": row["Id"], "Название": row["Название"], "Тело": row["Тело"], "Дата" : row["Дата"]})
-    os.remove("notes.csv")
-    os.rename("new_notes.csv", "notes.csv")
+# def edit_note():
+#     del_Id = input('Введите Id: ')
+#     with open("notes.csv", encoding='utf-8') as f:
+#         file = csv.DictReader(f, delimiter = ";")
+#         new_title = input('Введите новое название: ')
+#         new_body = input('Введите новое тело заметки: ')
+#         new_date = datetime.datetime.now().strftime("%Y-%m-%d")
+#         with open("new_notes.csv", mode="w", encoding='utf-8') as ff:
+#                 new_entry = ["Id", "Название","Тело", "Дата"]
+#                 ffile = csv.DictWriter(ff, delimiter = ";", 
+#                                             lineterminator="\r", fieldnames=new_entry)
+#                 file.writeheader()
+#                 file.writerow({"Id": del_Id, "Название": new_title, "Тело": new_body, "Дата" : new_date})
+#         for row in file:        
+#             if row["Id"] != del_Id and row["Id"] != 'Id':
+#                 with open("new_notes.csv", mode="a", encoding='utf-8') as w_file:
+#                     new_entry = ["Id", "Название","Тело", "Дата"]
+#                     file_writer = csv.DictWriter(w_file, delimiter = ";", 
+#                                             lineterminator="\r", fieldnames=new_entry)
+#                     file_writer.writeheader()
+#                     file_writer.writerow({"Id": row["Id"], "Название": row["Название"], "Тело": row["Тело"], "Дата" : row["Дата"]})
+#     os.remove("notes.csv")
+#     os.rename("new_notes.csv", "notes.csv")
 
 
-def filter_of_date():
-    date = input('Введите дату: ')
-    with open("notes.csv", encoding='utf-8') as r_file:
-        file_reader = csv.DictReader(r_file, delimiter = ";")
-        for row in file_reader:
-            if row["Дата"] == date:
-                for key, value in row.items():
-                    print("{0}: {1}".format(key,value))
-                print('-----------------------------------')
+# def filter_of_date():
+#     date = input('Введите дату: ')
+#     with open("notes.csv", encoding='utf-8') as r_file:
+#         file_reader = csv.DictReader(r_file, delimiter = ";")
+#         for row in file_reader:
+#             if row["Дата"] == date:
+#                 for key, value in row.items():
+#                     print("{0}: {1}".format(key,value))
+#                 print('-----------------------------------')
 
 
-def print_note():
-    id = input('Введите Id записи: ')
-    with open("notes.csv", encoding='utf-8') as r_file:
-        file_reader = csv.DictReader(r_file, delimiter = ";")
-        for row in file_reader:
-            if row["Id"] == id:
-                for key, value in row.items():
-                    print("{0}: {1}".format(key,value))
-                print('-----------------------------------')   
+# def print_note():
+#     id = input('Введите Id записи: ')
+#     with open("notes.csv", encoding='utf-8') as r_file:
+#         file_reader = csv.DictReader(r_file, delimiter = ";")
+#         for row in file_reader:
+#             if row["Id"] == id:
+#                 for key, value in row.items():
+#                     print("{0}: {1}".format(key,value))
+#                 print('-----------------------------------')   
    
--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from Loggers import read_notes, add_note, del_note, edit_note, filter_of_date, print_note
 
 def main():
-    file_name = 'notes.json'
-    notes = read_notes(file_name)
     print('Для работы с программой выберите нужное действие:')
     print('1 - вывести все заметки\n'
             '2 - создать новую заметку\n'
@@ -10,25 +8,25 @@ def main():
             '4 - удалить заметку\n'
             '5 - сделать выборку по дате\n'
             '6 - вывести заметку\n'
-            '5 - завершение работы\n')
+            '7 - завершение работы\n')
     while True:
         comand  = int(input('Введите цифру, соответствующую команде:'))
-        if comand not in (1, 2, 3, 4, 5):
+        if comand not in (1, 2, 3, 4, 5, 6, 7):
             print('Такой команды не существует!')
             print(' ')
             main()
         elif comand == 1:
-            read_notes
+            read_notes()
         elif comand == 2:
-            add_note
+            add_note()
         elif comand == 3:
-            edit_note
+            edit_note()
         elif comand == 4:
-            del_note
+            del_note()
         elif comand == 5:
-            filter_of_date
+            filter_of_date()
         elif comand == 6:
-            print_note
+            print_note()
         elif comand == 7:
             return
         
