b4c298a7d897984006f455ffe29a91b9f1939d6a
Olga Charushnikova <olga_charushnikova@bk.ru>
2023-10-17 00:34:32 +0300

    Удаление заметки

--- Loggers.py
+++ Loggers.py
@@ -24,4 +24,21 @@ def add_note():
         new_body = input('Введите тело заметки: ')
         new_date = datetime.datetime.now().strftime("%Y-%m-%d")
         file.writeheader()
-        file.writerow({"Id": new_Id, "Название": new_title, "Тело": new_body, "Дата" : new_date})    
+        file.writerow({"Id": new_Id, "Название": new_title, "Тело": new_body, "Дата" : new_date})  
+
+
+
+def del_note():
+    del_Id = input('Введите Id: ')
+    with open("notes.csv", encoding='utf-8') as f:
+        file = csv.DictReader(f, delimiter = ";")
+        for row in file:
+            if row["Id"] != del_Id and row["Id"] != 'Id':
+                with open("new_notes.csv", mode="a", encoding='utf-8') as ff:
+                    new_entry = ["Id", "Название","Тело", "Дата"]
+                    ffile = csv.DictWriter(ff, delimiter = ";", 
+                                                lineterminator="\r", fieldnames=new_entry)
+                    ffile.writeheader()
+                    ffile.writerow({"Id": row["Id"], "Название": row["Название"], "Тело": row["Тело"], "Дата" : row["Дата"]})
+    os.remove("notes.csv")
+    os.rename("new_notes.csv", "notes.csv")  
