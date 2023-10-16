d5b020218aa0a0f046d4948c1b2760d2896a32fd
Olga Charushnikova <olga_charushnikova@bk.ru>
2023-10-17 00:50:20 +0300

    Запрос команды

---
+++ main.py
@@ -0,0 +1,36 @@
+from Loggers import read_notes, add_note, del_note, edit_note, filter_of_date, print_note
+
+def main():
+    file_name = 'notes.json'
+    notes = read_notes(file_name)
+    print('Для работы с программой выберите нужное действие:')
+    print('1 - вывести все заметки\n'
+            '2 - создать новую заметку\n'
+            '3 - редактировать заметку\n'
+            '4 - удалить заметку\n'
+            '5 - сделать выборку по дате\n'
+            '6 - вывести заметку\n'
+            '5 - завершение работы\n')
+    while True:
+        comand  = int(input('Введите цифру, соответствующую команде:'))
+        if comand not in (1, 2, 3, 4, 5):
+            print('Такой команды не существует!')
+            print(' ')
+            main()
+        elif comand == 1:
+            read_notes
+        elif comand == 2:
+            add_note
+        elif comand == 3:
+            edit_note
+        elif comand == 4:
+            del_note
+        elif comand == 5:
+            filter_of_date
+        elif comand == 6:
+            print_note
+        elif comand == 7:
+            return
+        
+if __name__ == '__main__':
+      main()
